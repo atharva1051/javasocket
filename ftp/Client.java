bc58ab7a1793bef991831f193a8f9a32220d22ee
Atharva Patil <atharvapatil@Atharvas-MacBook-Air.local>
2022-05-15 16:42:41 +0530

    ftp done

--- ftp/Client.java
+++ ftp/Client.java
@@ -4,7 +4,7 @@ public class Client {
     public static void main(String args[]) throws Exception{
         Socket s= new Socket("127.0.0.1",7777);
         if(s.isConnected()){
-            System.out.println("onnected to server");
+            System.out.println("Connected to server");
         }
         FileOutputStream fout=new FileOutputStream("received.txt");
         DataInputStream din= new DataInputStream(s.getInputStream());
